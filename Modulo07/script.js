ce43c49863fc1b983ec2a88fbffd723ca61e5649
Jonathan Muller <jonathanmuller105@gmail.com>
2023-09-19 22:47:13 -0300

    19/09

---
+++ Modulo07/script.js
@@ -0,0 +1,9 @@
+let nomecandidato = document.getElementById(`nome-candidato`)
+nomecandidato.value = Jonathan Muller
+nomecandidato.type 
+nomecandidato.id 
+nomecandidato.required 
+nomecandidato.placeholder 
+let numeromatricula = document.getElementById(`numero-matricula`)
+numeromatricula.value = `um`
+numeromatricula.value = 10
